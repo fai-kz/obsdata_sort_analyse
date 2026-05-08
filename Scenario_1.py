91eb0f0dfe3561ba589e432e9b01e718a1c6ba65
Nurken28 <126747750+Nurken28@users.noreply.github.com>
2026-05-08 10:35:20 +0600

    Update Scenario_1.py

--- Scenario_1.py
+++ Scenario_1.py
@@ -40,12 +40,12 @@ def ensure_dirs(base: Path):
         (calib / t).mkdir(exist_ok=True)
 
 
-def process_assy_directory(base_dir: Path):
+def process_directory(base_dir: Path):
     base_dir = Path(base_dir)
 
     print(f"[Scan] scan path: {base_dir}")
 
-    fits_found = [p for p in base_dir.rglob("*") if p.suffix.lower() in (".fits", ".fit", ".fts")]
+    fits_found = [p for p in base_dir.rglob("*") if p.suffix.lower() in (".fits", ".fit")]
 
     print(f"[Info] Found FITS files: {len(fits_found)}")
 
@@ -84,7 +84,106 @@ def process_assy_directory(base_dir: Path):
             print(f"[Error] moving error: {e}")
 
 
-base = Path(r"C:\Users\Nurken\Desktop\python\ML\Observation test\Assy\azt-20\Calibration\2021\2021-01-05")
-process_assy_directory(base)
+
+
+def is_date_folder(name: str):
+    return len(name) == 10 and name[4] == "-" and name[7] == "-"
+
+
+def process_telescope(telescope_path: Path):
+
+    # создаём папку "observ_mode" в директории "...\Observation\observatory_id\instrument_id"
+    # создаём папки calibration и science в папке observ_mode
+    observ_mode_dir = telescope_path / "observ_mode"
+    calib_out = observ_mode_dir / "calibration"
+    science_out = observ_mode_dir / "science"
+
+    calib_out.mkdir(parents=True, exist_ok=True)
+    science_out.mkdir(parents=True, exist_ok=True)
+
+    # поиск observation mode 
+    for mode_dir in telescope_path.iterdir():
+
+        if not mode_dir.is_dir() or mode_dir.name == "observ_mode":
+            continue
+
+        # ищем год
+        for year_dir in mode_dir.iterdir():
+
+            if not year_dir.is_dir():
+                continue
+
+            # ищем даты
+            for date_dir in year_dir.iterdir():
+
+                if not date_dir.is_dir() or not is_date_folder(date_dir.name):
+                    continue
+
+                date_name = date_dir.name
+                year_name = year_dir.name
+
+                print(f" Папка даты: {date_name}")
+
+            
+                # Перемещение папок Calibration
+            
+
+                calib_src = date_dir / "calib"
+
+                if calib_src.exists():
+
+                    calib_target = calib_out / year_name / date_name
+                    calib_target.mkdir(parents=True, exist_ok=True)
+
+                    for sub in ["flat", "bias", "dark", "lamp"]:
+
+                        # src = source - папка из которой перемещают
+                        src = calib_src / sub
+
+                        if src.exists():
+
+                            # end_d = end_directory - папка в которую перемещают
+                            end_d = calib_target / sub
+
+                            if not end_d.exists():
+                                shutil.move(str(src), str(end_d))
+                                print(f" Перемещено: из {src} в {end_d}")
+                            else:
+                                print(f" Папка уже существует: {end_d}")
+
+
+                # Перемещение папок Science
+
+                science_src = date_dir / "science"
+
+                if science_src.exists():
+
+                    science_target = science_out / year_name / date_name
+                    science_target.mkdir(parents=True, exist_ok=True)
+
+                    end_d = science_target / "science"
+
+                    if not end_d.exists():
+                        shutil.move(str(science_src), str(end_d))
+                        print(f" Перемещено: из {src} в {end_d}")
+                    else:
+                        print(f" Папка уже существует: {end_d}")
+            
+base = Path(r"paste the directory to the folder with files")
+process_directory(base)
+
+# корневая папка | нужно указывать папку "Observation"
+BASE_DIR = Path(r"paste the directory")
+
+for obs_dir in BASE_DIR.iterdir():
+
+    if not obs_dir.is_dir():
+        continue
+
+    # observatory_id 
+    for telescope_dir in obs_dir.iterdir():
+
+        if telescope_dir.is_dir():
+            process_telescope(telescope_dir)
 
 
